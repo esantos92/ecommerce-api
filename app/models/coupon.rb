2bdcf621bb7b747607f7f333bad4595a7a9320fc
Everton Santos <everton.santos@redealumni.com>
2023-03-10 18:42:36 -0300

    :construction: build: Added some tests to Coupon due_date

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,8 +1,8 @@
 class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
-
-  validates :status, :due_date, presence: true
+  validates :status, presence: true
+  validates :due_date, presence: true, future_date: true
 
   enum status: { active: 1, inactive: 2 }
 end

afa5ef33ddaf28024ad91f2a9448f9d4f13fd36b
Everton Santos <everton.santos@redealumni.com>
2023-03-10 09:24:54 -0300

    :construction: build: Model, Spec and Factory to Product initiated

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+  belongs_to :productable, polymorphic: true
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+end

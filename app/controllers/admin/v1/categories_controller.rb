46406dd503f3285d7221e1082fd5ca1e99a4bc39
Everton Santos <everton.santos@redealumni.com>
2023-03-20 19:17:10 -0300

    :construction: build: Routes do systems requirements implemented

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -2,7 +2,7 @@ module Admin::V1
   class CategoriesController < ApiController
     before_action :load_category, only: [:update, :destroy]
 
-    def  index
+    def index
       @categories = Category.all
     end
 
